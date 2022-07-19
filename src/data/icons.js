b83ad8c1845a969dad8bb238201ccf7723f24b44
Alex Incerti <alex.incerti@outlook.com>
2022-07-19 21:47:03 +0200

    fix vkr icon

--- src/data/icons.js
+++ src/data/icons.js
@@ -3,7 +3,7 @@ export const icons = {
     'axlUSDT': 'https://app.astroport.fi/tokens/usdt.svg',
     'LUNA': 'https://assets.terra.money/icon/svg/LUNA.png',
     'ASTRO': 'https://app.astroport.fi/tokens/astro.png',
-    'VKR': 'https://app.astroport.fi/tokens/vkr.svg',
+    'VKR': 'https://app.astroport.fi/tokens/vkr.jpg',
     'LunaX': 'https://app.astroport.fi/tokens/lunax.png',
     'STEAK':'https://app.astroport.fi/tokens/steak.png',
     'ampLUNA': 'https://app.astroport.fi/tokens/ampLuna.png'
